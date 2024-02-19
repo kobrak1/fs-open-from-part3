e921d3a60aa4bd37f7aa44330847afc769933a1f
kobrak1 <burakkarhan657@gmail.com>
2024-02-19 11:56:29 +0300

    3.2: Phonebook backend step 2

--- api/index.js
+++ api/index.js
@@ -1,36 +1,49 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // user data list
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
 // route to handle get requests to /api/persons
-app.get('/api/persons', (req, res) =>{
-  res.json(persons)
-})
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
+
+app.get("/info", (req, res) => {
+  const currentDate = new Date();
+  const currentTime = currentDate.toTimeString();
+  const userCount = persons.length;
+
+  const info = `
+  <p>Phonebook has info for ${userCount} people</p> <br />
+  <p> ${currentTime} </p>
+  `;
+
+  res.send(info)
+});
 
 //start the server
-const PORT = 5001
-app.listen(PORT)
-console.log(`server is running on port ${PORT}`)
+const PORT = 5001;
+app.listen(PORT);
+console.log(`server is running on port ${PORT}`);

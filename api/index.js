dc4732456ec715b7f3260f9dd575fb812ea25150
kobrak1 <burakkarhan657@gmail.com>
2024-03-01 17:57:05 +0300

    3.14: Phonebook database, step 2

--- api/index.js
+++ api/index.js
@@ -1,29 +1,20 @@
 require("dotenv").config();
 const express = require("express");
-const morgan = require('morgan');
-const cors = require('cors');
-const Person = require('./models/person')
+const morgan = require("morgan");
+const cors = require("cors");
+const Person = require("./models/person");
 
-// assign express() to the variable called 'app'
-const app = express();
-
-// middleware to parse JSON bodies
-app.use(express.json());
-
-// middleware to assure same origin policy
-app.use(cors());
-
-// Set up Morgan middleware to log requests
-app.use(morgan('dev'))
-
-// middleware to vies static files on backend
-app.use(express.static('dist-api'));
+const app = express(); // assign express() to the variable called 'app'
+app.use(express.json()); // middleware to parse JSON bodies
+app.use(cors()); // middleware to assure same origin policy
+app.use(morgan("dev")); // Set up Morgan middleware to log requests
+app.use(express.static("dist-api")); // middleware to view static files on backend
 
 // route to handle get requests to /api/persons
 app.get("/api/persons", (req, res) => {
-  Person.find({}).then(result => {
-    res.json(result)
-  })
+  Person.find({}).then((result) => {
+    res.json(result);
+  });
 });
 
 // show info about the persons list
@@ -34,8 +25,7 @@ app.get("/info", (req, res) => {
 
   const info = `
   <p>Phonebook has info for ${userCount} people</p> <br />
-  <p> ${currentTime} </p>
-  `;
+  <p> ${currentTime} </p>`;
 
   res.send(info);
 });
@@ -52,50 +42,31 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
-// delete the specified data
+// DELETE the specified data
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  const newPersons = Person.filter((p) => p.id !== id);
 
-  if (newPersons === Person) {
-    return res
-      .status(404)
-      .send(`There is no any person with the specified id: ${id}`);
-  } else {
-    res.json(newPersons);
-  }
-  res.status(204).end();
+  Person.findByIdAndDelete(id, (err, docs) => {
+    if (err) {
+      console.log("Error:", err);
+    } else {
+      console.log("Deleted:", docs);
+    }
+  });
 });
 
-// post a new person
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-  return maxId + 1;
-};
-
-// post a new person
+// POST a new person
 app.post("/api/persons", (req, res) => {
   const body = req.body;
-  const nameCheck = Person.find(p => p.name === body.name)
-
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "Name or number is missing",
-    });
-  } else if (nameCheck) {
-    return res.status(400).json({
-      error: "There is already a person with the same name."
-    })
-  }
 
   const personInfo = new Person({
     name: body.name,
-    number: body.number
-  })
+    number: body.number,
+  });
 
-  personInfo.save().then(savedPerson => {
-    res.json(savedPerson)
-  })
+  personInfo.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 //start the server
